d2ba3b63efd7dc1d3890ef0ddd471b81ab9dc303
Ahmad2003-star <fousseniahamadou94@gmail.com>
2026-06-11 23:23:27 +0100

    Ajout du script de creation automatique de l'admin

---
+++ backend/createsuperuser.py
@@ -0,0 +1,14 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configuration.settings')
+django.setup()
+
+from authentification.models import Utilisateur # Adaptez si votre modèle personnalisé est ailleurs
+
+# On vérifie si l'admin existe déjà pour ne pas créer de doublon
+if not Utilisateur.objects.filter(username='admin').exists():
+    Utilisateur.objects.create_superuser('admin', 'admin@example.com', 'motdepass')
+    print("Superutilisateur créé avec succès !")
+else:
+    print("L'admin existe déjà.")
