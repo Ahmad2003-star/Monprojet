bc7bf8f055f35168306529e2fdfa7c403ac9924f
Ahmad2003-star <fousseniahamadou94@gmail.com>
2026-06-12 00:17:20 +0100

    Correction de la route du token pour correspondre au frontend

--- backend/configuration/urls.py
+++ backend/configuration/urls.py
@@ -5,7 +5,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/', include('authentification.urls')),
     path('api/academique/', include('academique.urls')),
 ]
