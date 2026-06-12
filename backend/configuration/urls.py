e03bd52e9ccacbc3476e5f49ccf978b8828217c5
Ahmad2003-star <fousseniahamadou94@gmail.com>
2026-06-12 22:28:53 +0100

    Ajout de la route racine pour le Health Check de Render

--- backend/configuration/urls.py
+++ backend/configuration/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import HttpResponse # <-- Ligne ajoutée
+
+# Petite fonction de secours pour Render
+def home(request):
+    return HttpResponse("API FAST Natitingou en ligne", content_type="text/plain")
 
 urlpatterns = [
+    path('', home), # <-- Ligne ajoutée pour la racine
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
